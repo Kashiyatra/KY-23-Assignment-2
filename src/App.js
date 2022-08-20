b624584df71d999bad07b8eb61a29c59ad876e1a
Archit Pattanaik <98776672+archit-2003@users.noreply.github.com>
2022-08-21 01:12:56 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,70 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className='Header'> FAQ </div>
+    <div className="items">
+      <div className="card">
+        <div className="card-content">
+          <h2 className="card-title">Question here</h2>
+          <p className="card-body">Answer to that Respective quetion</p>
+          <a href="#" className="button">
+            Click Here
+          </a>
+        </div>
+      </div>
+      <div className="card">
+        <div className="card-content">
+          <h2 className="card-title">Question here</h2>
+          <p className="card-body">Answer to that Respective quetion</p>
+          <a href="#" className="button">
+            Click Here
+          </a>
+        </div>
+      </div>
+      <div className="card">
+        <div className="card-content">
+          <h2 className="card-title">Question here</h2>
+          <p className="card-body">Answer to that Respective quetion</p>
+          <a href="#" className="button">
+            Click Here
+          </a>
+        </div>
+      </div>
+    </div>
+    <div className="items">
+      <div className="card">
+        <div className="card-content">
+          <h2 className="card-title">Question here</h2>
+          <p className="card-body">Answer to that Respective quetion</p>
+          <a href="#" className="button">
+            Click Here
+          </a>
+        </div>
+      </div>
+      <div className="card">
+        <div className="card-content">
+          <h2 className="card-title">Question here</h2>
+          <p className="card-body">Answer to that Respective quetion</p>
+          <a href="#" className="button">
+            Click Here
+          </a>
+        </div>
+      </div>
+      <div className="card">
+        <div className="card-content">
+          <h2 className="card-title">Question here</h2>
+          <p className="card-body">Answer to that Respective quetion</p>
+          <a href="#" className="button">
+            Click Here
+          </a>
+        </div>
+      </div>
+    </div>
+  </>
+  );
+}
+
+export default App;
