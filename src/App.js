513b1d5730babb3f33c62093d771611fca89d6e5
Samar_Singh_Randhawa <samarsinghrandhawa11@gmail.com>
2022-08-21 00:28:47 +0530

    Made changes required for Theme

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { motion } from "framer-motion";
+import AnimatedText from "./AnimatedText";
+import "./styles.css";
+
+export default function App() {
+
+  // Placeholder text data, as if from API
+  const placeholderText = [
+    { type: "heading1", text: "KASHIYATRA 2022" },
+    {
+      type: "heading2",
+      text: "IIT (BHU)VARANASI"
+    }
+  ];
+  const paragraphText=[
+    {
+      type: "paragraph",
+      text: "Kashiyatra is housed in the holy city of Varanasi, the city older than history and with a cultural canvas as intricate as its gullies and offers a window into the hidden turnings of the city's soul. Owing to the ongoing global pandemic, Kashiyatra this year resorted to the online medium. Its sponsors include some of the biggest companies like Ultratech, 9xm, Daily Hunt, Growfitter, Spartan Poker and SECL, opening the doors to its events to countless students who couldn't have attended otherwise."
+    }
+  ]
+
+  const container = {
+    visible: {
+      transition: {
+        staggerChildren: 0.025
+      }
+    }
+  };
+
+
+
+  return (
+    <>
+    <motion.div
+      className="App"
+      initial="hidden"
+      // animate="visible"
+      // animate={true}
+      animate="visible" 
+      variants={container}
+    >
+      <div className="container">
+        {placeholderText.map((item, index) => {
+          return <AnimatedText {...item} key={index} />;
+        })}
+      </div>
+      <div className="container">
+        {paragraphText.map((item, index) => {
+          return <AnimatedText {...item} key={index} />;
+        })}
+      </div>
+      {/* <button onClick={handleReplay}>
+        Replay <span>⟲</span>
+      </button> */}
+    </motion.div>
+   
+    </>
+    
+  );
+}
---
+++ src/Shape.js
@@ -0,0 +1,35 @@
+import { useEffect } from "react";
+import { motion, useCycle } from "framer-motion";
+const itemsA = [1, 2, 3, 4];
+const itemsB = [3, 1, 4, 2];
+const itemsC = [4, 3, 2, 1];
+const itemsD = [2, 4, 1, 3];
+
+const colors = ["#caf0f8", "#ade8f4", "#90e0ef", "#48cae4"];
+
+
+export default function Shape(){
+    const [items, setItems] = useCycle(itemsA, itemsB, itemsC, itemsD);
+
+    useEffect(() => {
+        setTimeout(() => setItems(), 1500);
+    }, [items, setItems]);
+
+    return (
+        <div className="squares">
+            {items.map((item) => (
+                <motion.div
+                className="smallSquare"
+                    style={{
+                        
+                        backgroundColor: colors[item - 1]
+                    }}
+                    key={item}
+                    layout
+                    transition={{ type: "spring", stiffness: 350, damping: 25 }}
+                />
+            ))}
+        </div>
+    );
+
+}
