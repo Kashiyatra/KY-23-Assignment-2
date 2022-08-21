7a3ba139733fb8ed839e361cff6692283b3bed12
ashish7515 <96697897+ashish7515@users.noreply.github.com>
2022-08-22 00:05:42 +0530

    Add files via upload

---
+++ src/faqlibrary.js
@@ -0,0 +1,64 @@
+var faqLibrary = [
+  {
+    id: 1,
+    question: "What is ........... ?",
+    content:
+      "Bacon ipsum dolor amet boudin kielbasa alcatra jowl salami, pig porchetta prosciutto ham hock pork loin drumstick meatball picanha ham. Rump capicola ground round spare ribs turkey beef short ribs. Tenderloin shank andouille brisket capicola spare ribs rump meatloaf picanha cupim sirloin.",
+  },
+
+  {
+    id: 2,
+    question: "How do I ......... ?",
+    content:
+      "Bacon ipsum dolor amet jerky meatloaf fatback strip steak chicken pastrami. Tri-tip short ribs prosciutto pork belly sirloin ball tip. Leberkas drumstick frankfurter turkey tri-tip filet mignon. Bresaola strip steak tail rump filet mignon cow, drumstick pig chicken bacon capicola doner ham cupim.",
+  },
+
+  {
+    id: 3,
+    question: "How does ....... ?",
+    content:
+      "Ham fatback turducken brisket, leberkas biltong venison landjaeger turkey flank bresaola alcatra ribeye. Porchetta corned beef jerky chuck ham hock filet mignon strip steak pork loin hamburger tongue.",
+  },
+
+  {
+    id: 4,
+    question: "How can we ........ ?",
+    content:
+      "Bacon ipsum dolor amet venison strip steak capicola short loin pancetta. Turkey jowl frankfurter drumstick ham hock cow doner jerky leberkas landjaeger turducken prosciutto. Ribeye short ribs venison jerky capicola shank, pork chop andouille ground round boudin picanha corned beef swine chuck.",
+  },
+
+  {
+    id: 5,
+    question: "The site won't load for me. What can I do?",
+    content:
+      "Pork loin pastrami frankfurter boudin picanha, shank tri-tip chuck beef swine strip steak pork chop jerky. Filet mignon hamburger doner, beef pork belly cow swine prosciutto tri-tip porchetta tenderloin. Brisket alcatra frankfurter pancetta pork chop doner chicken.",
+  },
+
+  {
+    id: 6,
+    question: "Why is my profile not showing up correctly?",
+    content:
+      "Spare ribs brisket flank prosciutto landjaeger salami chuck short ribs tri-tip beef ribs filet mignon tongue kevin ham strip steak. Chicken ribeye swine kevin tri-tip prosciutto porchetta picanha flank venison ham short ribs corned beef filet mignon short loin. ",
+  },
+
+  {
+    id: 7,
+    question: "Why are certain parts of the site ........ ?",
+    content:
+      "Frankfurter brisket ribeye, strip steak pork loin tail alcatra beef. Salami tenderloin tri-tip, kevin beef ribs bacon sausage chicken meatloaf ham. Pork chop venison prosciutto strip steak ball tip tongue cow short loin.",
+  },
+
+  {
+    id: 8,
+    question: "How do I ........ ?",
+    content:
+      "Brisket tail boudin, bacon turducken frankfurter ground round swine cow meatloaf alcatra turkey venison bresaola meatball. Porchetta pork chop bresaola boudin jerky salami. Pork chop cow bacon jerky. Turkey turducken capicola flank strip steak, tenderloin spare ribs short loin. Beef ribs ribeye bacon kielbasa pork chop pork kevin pastrami shankle.",
+  },
+
+  {
+    id: 9,
+    question: "Do I need to register on the website?",
+    content:
+      "Prosciutto venison pork chop, doner beef ribs tail turducken kielbasa bacon rump bresaola. Leberkas strip steak picanha pancetta, fatback pork cupim biltong. Swine tail pork, pork loin rump biltong pancetta shank ham hock ball tip kevin salami chicken shankle. ",
+  },
+];
