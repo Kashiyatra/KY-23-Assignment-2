d450caba5045755e9793ebe3530e619b66b40bda
deepaksai313 <majjiga.deepaksai.ece21@itbhu.ac.in>
2022-08-21 19:50:22 +0530

    about us

---
+++ about.jsx
@@ -0,0 +1,7 @@
+var f1 = document.getElementsByClassName("e1");
+var f2 = document.getElementByClassName("e2");
+var f3 = document.getElementByClassName("e3");
+
+f1.textContent = 70000;
+f3.textContent = 60;
+f2.textContent = 350;
