11edff43ca1d9d128a178d7c0b46b91bdce0c951
AnantJain05 <74446727+AnantJain05@users.noreply.github.com>
2022-08-21 22:05:24 +0530

    Add files via upload

---
+++ src/components/title/title.js
@@ -0,0 +1,11 @@
+import "./title.css"
+
+const Heading = () => {
+    return (
+        <div className="header-box">
+            <h1 className="heading-title"> FAQ </h1>
+        </div>
+    );
+}
+
+export default Heading;
