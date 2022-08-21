de7534678cb2cd07c4fd68d1de068bef6817ae9d
AnantJain05 <74446727+AnantJain05@users.noreply.github.com>
2022-08-21 21:56:39 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Heading from './components/title/title';
+import Main from './components/main/main';
+
+function App() {
+  return (
+    <>
+      <Heading/>
+        <Main />
+    </>
+  );
+}
+
+export default App;
