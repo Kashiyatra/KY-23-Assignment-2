90dd68e565053ff7032a98f849da91fdd97d77d2
Samar1110 <98534735+Samar1110@users.noreply.github.com>
2022-08-22 19:27:29 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Shape from "./Shape"
 import App from "./App";
+import Waveform from "./Waveform";
+
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
     <App />
     <Shape />
+    <Waveform />
   </React.StrictMode>,
   rootElement
 );
+ 
