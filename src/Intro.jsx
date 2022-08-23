0555921892b171a813fecf7bf60359d100759ff2
atulyadav745 <106299466+atulyadav745@users.noreply.github.com>
2022-08-23 11:43:47 +0530

    done

---
+++ src/Intro.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function Intro() {
+  return (
+    <>
+     
+        <div className="heading">
+         <h1>02</h1>
+        </div>
+      
+    </>
+  );
+}
+export default Intro;
